7213938032641a9bfb87f7f59e16e8f44f2d512b
Phil Choi <pil0709123@gmail.com>
2023-10-25 23:43:54 +0900

    리덕스 예제 시작

---
+++ src/pages/use-redux/index.tsx
@@ -0,0 +1,16 @@
+const defaultPage = () => {
+  
+  return (
+    <section>
+      <h1>Clicked : times</h1>
+      <button style={{backgroundColor:'#fff',padding:'10px',marginInline:'5px'}}>
+        +
+      </button>
+      <button style={{backgroundColor:'#fff',padding:'10px',marginInline:'5px'}}>
+        -
+      </button>
+    </section>
+  );
+};
+
+export default defaultPage;
