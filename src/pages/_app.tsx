4b994d23eb59689917e88e367e1f999405c421d3
Phil Choi <pil0709123@gmail.com>
2023-10-27 00:24:30 +0900

    createStor 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,17 @@ import type { AppProps } from 'next/app';
 
 import Head from '@/components/common/Head';
 import { globalStyles } from '@/styles/reset';
+import { createStore } from 'redux';
+import counter from '@/reducers';
+import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const store = createStore(counter);
   return (
-    <>
+    <Provider store={store}>
       <Global styles={globalStyles} />
       <Head />
-      <Component {...pageProps} />
-    </>
+      <Component {...pageProps} value={store.getState()} onIncrement={() => store.dispatch({type:'INCREMENT'})} onDecrement={() => store.dispatch({type:'DECREMENT'})}/>
+    </Provider>
   );
 }
--- src/pages/use-redux/index.tsx
+++ src/pages/use-redux/index.tsx
@@ -1,12 +1,19 @@
-const defaultPage = () => {
-  
+type Props = {
+  value: number;
+  onIncrement: () => void;
+  onDecrement: () => void;
+}
+
+const defaultPage = ({value , onIncrement, onDecrement}: Props) => {
+  console.log(value);
+  console.log('sss',onIncrement);
   return (
     <section>
-      <h1>Clicked : times</h1>
-      <button style={{backgroundColor:'#fff',padding:'10px',marginInline:'5px'}}>
+      <h1>Clicked : {value} times</h1>
+      <button style={{backgroundColor:'#fff',padding:'10px',marginInline:'5px'}} onClick={onIncrement}>
         +
       </button>
-      <button style={{backgroundColor:'#fff',padding:'10px',marginInline:'5px'}}>
+      <button style={{backgroundColor:'#fff',padding:'10px',marginInline:'5px'}} onClick={onDecrement}>
         -
       </button>
     </section>
