7f5058d83aa13521f1a0b406024a9a298ffb4243
Phil Choi <pil0709123@gmail.com>
2023-11-07 23:49:11 +0900

    todo added

--- src/pages/use-redux/index.tsx
+++ src/pages/use-redux/index.tsx
@@ -2,6 +2,8 @@ import { useSelector, useDispatch } from 'react-redux';
 
 const UseReduxPage = () => {
   const value = useSelector((state: {counter: number}) => state.counter);
+  const todoValue = useSelector((state: {todos: string[]}) => state.todos);
+  console.log('todoValue :::: ',todoValue);
   const dispatch = useDispatch();
 
   const onIncrement = () => {
@@ -12,6 +14,10 @@ const UseReduxPage = () => {
     dispatch({ type: 'DECREMENT' });
   };
 
+  const changeTodo = (e) => {
+    dispatch({ type: 'ADD_TODO', text: e.target.value });
+  }
+
   return (
     <section>
       <h1>Clicked : {value} times</h1>
@@ -21,6 +27,7 @@ const UseReduxPage = () => {
       <button style={{backgroundColor:'#fff',padding:'10px',marginInline:'5px'}} onClick={onDecrement}>
         -
       </button>
+      <input value={todoValue} onChange={changeTodo} style={{color:'#fff'}}/>
     </section>
   );
 };
