ae3cee4284cff9df09c8682f85333994a2dfd213
Phil Choi <pil0709123@gmail.com>
2023-11-09 20:39:18 +0900

    eslint 설정 미완

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
-import '@/styles/globals.css';
+import "@/styles/globals.css";
 
-import { Global } from '@emotion/react';
-import type { AppProps } from 'next/app';
-import Head from '@/components/common/Head';
-import { globalStyles } from '@/styles/reset';
-import { Provider } from 'react-redux';
-import store from '@/store';
+import { Global } from "@emotion/react";
+import type { AppProps } from "next/app";
+import Head from "@/components/common/Head";
+import { globalStyles } from "@/styles/reset";
+import { Provider } from "react-redux";
+import store from "@/store";
 
 export default function App({ Component, pageProps }: AppProps) {
-  
   return (
     <Provider store={store}>
       <Global styles={globalStyles} />
