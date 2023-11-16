1e3e862f9d7a85c67c9aadf92d7ecf50e096b65c
Phil Choi <pil0709123@gmail.com>
2023-11-16 23:27:18 +0900

    업로드페이지 완성

--- src/pages/item/upload.tsx
+++ src/pages/item/upload.tsx
@@ -23,25 +23,46 @@ const Upload: NextPage = () => {
           <input type="file" className="hidden" />
         </label>
       </div>
-      <div>
-        <label htmlFor="price">Price</label>
-        <div>
-          <div>
-            <span>$</span>
+      <div className="my-5">
+        <label
+          className="text-sm mb-1 block font-medium text-gray-900"
+          htmlFor="price"
+        >
+          Price
+        </label>
+        <div className="rounded-md relative shadow-sm flex items-center">
+          <div className="absolute left-0 pl-3 flex items-center justify-center pointer-events-none">
+            <span className="text-gray-500 text-sm">$</span>
           </div>
-          <input id="price" type="text" placeholder="0.00" />
-          <div>
-            <span>USD</span>
+          <input
+            id="price"
+            className="pl-7 rounded-md appearance-none w-full px-3 py-2 border border-solid border-gray-300 rounded-r-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            type="text"
+            placeholder="0.00"
+          />
+          <div className=" absolute right-0 pr-3 flex items-center pointer-events-none">
+            <span className=" text-gray-500">USD</span>
           </div>
         </div>
       </div>
       <div>
-        <label>Description</label>
+        <label
+          className="text-sm mb-1 block font-medium text-gray-900"
+          htmlFor="description"
+        >
+          Description
+        </label>
         <div>
-          <textarea rows={4} />
+          <textarea
+            className="mt-1 shadow-sm w-full focus:outline-none  focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
+            id="description"
+            rows={4}
+          />
         </div>
       </div>
-      <button>Upload product</button>
+      <button className="mt-4 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md w-full shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
+        Upload product
+      </button>
     </div>
   );
 };
