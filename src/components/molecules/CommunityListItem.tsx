d5cf3e37d8eb1ab709ba1b80cfa634f717bf31fc
phil-0709 <126749898+phil-0709@users.noreply.github.com>
2023-11-22 10:01:05 +0900

    chat 아이콘 컴포넌트로 분리

--- src/components/molecules/CommunityListItem.tsx
+++ src/components/molecules/CommunityListItem.tsx
@@ -1,4 +1,5 @@
 import { CommunityListItemProps } from '@/types/types';
+import ChatIcon from '@/components/atom/icons/ChatIcon';
 
 const CommunityListItem = ({id, tag, title, userName, uploadTime, interestingCount, answerCount}:CommunityListItemProps) => {
   
@@ -33,20 +34,7 @@ const CommunityListItem = ({id, tag, title, userName, uploadTime, interestingCou
                 <span>궁금해요 {interestingCount}</span>
               </span>
               <span className="flex space-x-2 items-center text-sm">
-                <svg
-                  className="w-4 h-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                  ></path>
-                </svg>
+                <ChatIcon/>
                 <span>답변 {answerCount}</span>
               </span>
             </div>
