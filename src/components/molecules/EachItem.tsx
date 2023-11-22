905eb3580e0317f04180bb42378c33b24bcd7321
phil-0709 <126749898+phil-0709@users.noreply.github.com>
2023-11-22 10:03:20 +0900

    like 아이콘 컴포넌트 분리

--- src/components/molecules/EachItem.tsx
+++ src/components/molecules/EachItem.tsx
@@ -1,5 +1,6 @@
 import { EachItemProps } from '@/types/types';
 import ChatIcon from '@/components/atom/icons/ChatIcon';
+import LikeIcon from '@/components/atom/icons/LikeIcon';
 
 const EachItem = ({
   id,
@@ -23,20 +24,7 @@ const EachItem = ({
       </div>
       <div className="flex space-x-2 justify-end items-end">
         <div className="flex space-x-0.5 items-center text-sm text-gray-600">
-          <svg
-            className="w-4 h-4"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-            ></path>
-          </svg>
+          <LikeIcon/>
           <span>{likeCount}</span>
         </div>
         <div className="flex space-x-0.5 items-center text-sm text-gray-600">
