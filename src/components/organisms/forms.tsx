6c87eca56f0f1d65aaea6d4e30cf33f5a9673938
Phil Choi <pil0709123@gmail.com>
2023-11-27 23:24:21 +0900

    form 테스트

---
+++ src/components/organisms/forms.tsx
@@ -0,0 +1,53 @@
+import { useCallback } from 'react';
+import { useForm, SubmitHandler, SubmitErrorHandler } from 'react-hook-form';
+
+type UserInputs = {
+  name: string;
+  email: string;
+  password: string;
+  phone?: number;
+};
+
+const Forms = () => {
+  const { register, handleSubmit, watch, clearErrors } = useForm<UserInputs>();
+
+  const onSubmit: SubmitHandler<UserInputs> = (data) => {
+    console.log(data);
+  };
+
+  const onError: SubmitErrorHandler<UserInputs> = useCallback(
+    (formError) => {
+      if (formError.name?.type === 'required') {
+        alert('이름을 입력해 주세요.');
+      } else if (formError.email?.type === 'required') {
+        alert('이메일을 입력해 주세요.');
+      }
+      clearErrors();
+    },
+    [clearErrors],
+  );
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit, onError)}>
+      <input
+        type="text"
+        placeholder="User name"
+        {...register('name', { required: 'user name is required' })}
+      />
+      <input
+        type="email"
+        placeholder="User email"
+        {...register('email', { required: true })}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        {...register('password', { required: true })}
+      />
+      <input type="text" placeholder="User phone" {...register('phone')} />
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default Forms;
