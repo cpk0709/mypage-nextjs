cb0895f99987db10ed5095c7093e33d5d3f77a63
Phil Choi <pil0709123@gmail.com>
2023-12-03 22:44:46 +0900

    phone , email 정보조회및생성

--- src/pages/api/users/enter.ts
+++ src/pages/api/users/enter.ts
@@ -1,9 +1,25 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import withHandler from '@/libs/server/withHandler';
+import client from '@/libs/server/client';
+import { logger } from '@/utils/logger';
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('success', req.body.email);
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { email, phone } = req.body;
+  const payload = phone ? { phone: Number(phone) } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'Anonymous',
+      ...payload,
+    },
+    update: {},
+  });
+  logger.log('user result :::', user);
+
   res.status(200).end();
 }
 
