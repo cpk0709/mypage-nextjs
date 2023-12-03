dbe2a1db4b918045b1cfe436bbdb1ef3c66bb1a6
Phil Choi <pil0709123@gmail.com>
2023-12-04 00:27:03 +0900

    prisma update

--- src/pages/api/users/enter.ts
+++ src/pages/api/users/enter.ts
@@ -3,23 +3,37 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import withHandler from '@/libs/server/withHandler';
 import client from '@/libs/server/client';
-import { logger } from '@/utils/logger';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, phone } = req.body;
   const payload = phone ? { phone: Number(phone) } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  // const user = await client.user.upsert({
+  //   where: {
+  //     ...payload,
+  //   },
+  //   create: {
+  //     name: 'Anonymous',
+  //     ...payload,
+  //   },
+  //   update: {},
+  // });
+  const token = await client.token.create({
+    data: {
+      payload: '1234',
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: 'Anonymous',
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: 'Anonymous',
-      ...payload,
-    },
-    update: {},
   });
-  logger.log('user result :::', user);
-
+  console.log('token ::::', token);
   res.status(200).end();
 }
 
