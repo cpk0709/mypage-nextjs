e855211292ec39512dcbc0afec1870631e6fb379
Phil Choi <pil0709123@gmail.com>
2023-12-17 23:37:26 +0900

    aws 환경변수 적용

--- src/libs/server/email.ts
+++ src/libs/server/email.ts
@@ -1,16 +1,63 @@
+import { logger } from '@/utils/logger';
 import nodemailer from 'nodemailer';
 
-const smtptTransport = nodemailer.createTransport({
-  service: 'Naver',
-  host: 'smtp.naver.com',
-  port: 587,
-  auth: {
-    user: process.env.EMAIL_ID,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+const AWS = require('aws-sdk');
+const region = 'ap-northeast-2';
 
-export default smtptTransport;
+const ssm = new AWS.SSM({ region });
+
+async function getParameter(name: string): Promise<string> {
+  const params = {
+    Name: name,
+    WithDecryption: true,
+  };
+
+  try {
+    const response = await ssm.getParameter(params).promise();
+
+    return response.Parameter.Value;
+  } catch (error) {
+    logger.log('Error in getParameter:', error);
+
+    throw error;
+  }
+}
+// const smtptTransport = nodemailer.createTransport({
+//   service: 'Naver',
+//   host: 'smtp.naver.com',
+//   port: 587,
+//   auth: {
+//     user: process.env.EMAIL_ID,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+async function createSmtpTransport() {
+  try {
+    const emailId = await getParameter('my_blog_env_email_id');
+    const emailPassword = await getParameter('my_blog_env_email_password');
+
+    return nodemailer.createTransport({
+      service: 'Naver',
+      host: 'smtp.naver.com',
+      port: 587,
+      auth: {
+        user: emailId,
+        pass: emailPassword,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  } catch (error) {
+    logger.log('Error in createSmtpTransport:', error);
+
+    throw error;
+  }
+}
+
+export { createSmtpTransport };
--- src/pages/api/users/enter.ts
+++ src/pages/api/users/enter.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { NextApiRequest, NextApiResponse } from 'next';
 
@@ -5,10 +7,31 @@ import withHandler, { ResponseType } from '@/libs/server/withHandler';
 import client from '@/libs/server/client';
 import twilio from 'twilio';
 import { logger } from '@/utils/logger';
-// import smtpTransport from 'nodemailer/lib/smtp-transport';
-import smtptTransport from '@/libs/server/email';
+import { createSmtpTransport } from '@/libs/server/email';
 
-const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+const AWS = require('aws-sdk');
+const region = 'ap-northeast-2';
+
+const ssm = new AWS.SSM({ region });
+
+async function getParameter(name: string): Promise<string> {
+  const params = {
+    Name: name,
+    WithDecryption: true,
+  };
+
+  try {
+    const response = await ssm.getParameter(params).promise();
+
+    return response.Parameter.Value;
+  } catch (error) {
+    logger.log('Error in getParameter:', error);
+
+    throw error;
+  }
+}
+
+// const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -20,6 +43,15 @@ async function handler(
   if (!user) {
     return res.status(400).json({ ok: false });
   }
+
+  // AWS SSM으로부터 환경변수를 불러옵니다.
+  const twilioSid = await getParameter('my_blog_env_twilio_sid');
+  const twilioToken = await getParameter('my_blog_env_twilio_token');
+  const twilioMsid = await getParameter('my_blog_env_twilio_msid');
+  const myPhone = await getParameter('my_blog_env_my_phone');
+  const emailId = await getParameter('my_blog_env_email_id');
+
+  const twilioClient = twilio(twilioSid, twilioToken);
 
   const payload = String(Math.floor(100_000 + Math.random() * 900_000));
   const token = await client.token.create({
@@ -41,8 +73,10 @@ async function handler(
 
   if (phone) {
     const message = await twilioClient.messages.create({
-      messagingServiceSid: process.env.TWILIO_MSID,
-      to: process.env.MY_PHONE!,
+      // messagingServiceSid: process.env.TWILIO_MSID,
+      // to: process.env.MY_PHONE!,
+      messagingServiceSid: twilioMsid,
+      to: myPhone,
       body: `Your login token is ${payload}`,
     });
     logger.log('message :::', message);
@@ -50,19 +84,23 @@ async function handler(
 
   if (email) {
     const mailOptions = {
-      from: process.env.EMAIL_ID,
+      // from: process.env.EMAIL_ID,
+      from: emailId,
       to: email,
       subject: 'Nomad Carrot Authentication Email',
       text: `Authentication Code : ${payload}`,
     };
 
     try {
-      const result = await smtptTransport.sendMail(mailOptions);
+      // const result = await smtptTransport.sendMail(mailOptions);
+      const smtpTransport = await createSmtpTransport();
+      const result = await smtpTransport.sendMail(mailOptions);
+      smtpTransport.close();
     } catch (error) {
       logger.log(error);
     }
 
-    smtptTransport.close();
+    // smtptTransport.close();
   }
 
   res.json({ ok: true });
