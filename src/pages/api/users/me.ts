0a3c3d373cf017c6dd5a4fd96626af27714b4d9f
Phil Choi <pil0709123@gmail.com>
2024-01-14 23:56:09 +0900

    .

---
+++ src/pages/api/users/me.ts
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withIronSessionApiRoute } from 'iron-session/next';
+import withHandler from '@/libs/server/withHandler';
+import { logger } from '@/utils/logger';
+import client from '@/libs/server/client';
+
+declare module 'iron-session' {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
+const AWS = require('aws-sdk');
+const region = 'ap-northeast-2';
+
+const ssm = new AWS.SSM({ region });
+
+async function getParameter(name: string): Promise<string> {
+  const params = {
+    Name: name,
+    WithDecryption: true,
+  };
+
+  try {
+    const response = await ssm.getParameter(params).promise();
+
+    return response.Parameter.Value;
+  } catch (error) {
+    logger.log('Error in getParameter:', error);
+
+    throw error;
+  }
+}
+
+// const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  logger.log(req.session.user);
+  const profile = await client.user.findUnique({
+    where: {
+      id: req.session.user?.id,
+    },
+  });
+  // res.status(200).json({ ok: true });
+  res.json({
+    ok: true,
+    profile,
+  });
+}
+
+export default withIronSessionApiRoute(withHandler('GET', handler), {
+  cookieName: 'carrotsession',
+  password: 'sdkjn38n2ikfdnfoi_dsinfj328fsnfksajnkepgnkdfjngjks',
+});
