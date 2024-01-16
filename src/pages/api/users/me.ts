cdf3d89d3e3a2766b4fb0f5e3d5aa45ca396c367
Phil Choi <pil0709123@gmail.com>
2024-01-16 22:43:55 +0900

    fix : type error 수정

--- src/pages/api/users/me.ts
+++ src/pages/api/users/me.ts
@@ -38,12 +38,16 @@ async function getParameter(name: string): Promise<string> {
 
 // const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> {
   const profile = await client.user.findUnique({
     where: {
       id: req.session.user?.id,
     },
   });
+
   // res.status(200).json({ ok: true });
   res.json({
     ok: true,
