165c4b32f1b9331c643eb74e36fc5476c7bcbccc
Phil Choi <pil0709123@gmail.com>
2024-05-08 23:11:17 +0900

    second circle added animation applied

--- src/pages/css-challenge/stage_4/index.tsx
+++ src/pages/css-challenge/stage_4/index.tsx
@@ -12,6 +12,19 @@ const jumpJump1 = keyframes`
 	}
 
 `;
+
+const jumpJump2 = keyframes`
+	0%, 40% {
+		box-shadow: 2px 2px 3px 2px rgba(0,0,0,0.2);
+		transform: scale(0);
+	}
+
+	100% {
+		box-shadow: 10px 10px 15px 0 rgba(0,0,0,0.3);
+		transform: scale(1);
+	}
+`;
+
 const frame = css`
   position: absolute;
   top: 50%;
@@ -43,6 +56,18 @@ const center = css`
     border-radius: 50%;
     animation-fill-mode: both;
     animation: ${jumpJump1} 2s infinite alternate;
+  }
+  .dot-2 {
+    position: absolute;
+    z-index: 2;
+    width: 60px;
+    height: 60px;
+    top: 80px;
+    left: 80px;
+    background: #fff;
+    border-radius: 50%;
+    animation-fill-mode: both;
+    animation: ${jumpJump2} 2s infinite alternate;
   }
 `;
 
